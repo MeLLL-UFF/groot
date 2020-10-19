4644d3d61a6b40696e7260821bda4f2502a9b3e9
lefreire <letfreirefigueiredo@gmail.com>
2020-10-19 18:51:38 -0300

    Creating individuals and the population

---
+++ src/individual.py
@@ -0,0 +1,152 @@
+from boostsrl import boostsrl
+import copy
+from random import randint
+import re
+
+
+class Individual:
+
+	def __init__(self, tree_source, bk_source, bk_target, pred_target):
+		#pred_target é : pred_target = [('movie', '+,-'), ('director', '+'),...]
+		self.tree_source = tree_source
+		self.bk_source = bk_source
+		self.bk_target = bk_target
+		self.pred_target = pred_target
+		self.variables = []
+		self.new_bk_source = []
+		self.new_bk_target = []
+		self.transfer = []
+
+	def mapping_transfer(self, source_pred, target_pred):
+		#source é do tipo: pred(A) e target é pred
+		for pred in self.new_bk_source:
+			if source_pred.split('(')[0] in pred and 'source: {}'.format(pred) not in self.transfer: 
+				self.transfer.append('source: {}'.format(pred))
+				break
+		for pred in self.new_bk_target:
+			if target_pred in pred and 'target: {}'.format(pred) not in self.transfer:
+				self.transfer.append('target: {}'.format(pred))
+				break
+		self.transfer.append('setMap: {}={}({}'.format(source_pred, target_pred, 
+													   source_pred.split('(')[1]))
+
+	def generate_new_preds(self):
+		self.new_bk_source = copy.deepcopy(self.bk_source)
+		self.new_bk_target =copy.deepcopy(self.bk_target)
+
+		for index in range(0, len(self.new_bk_source)):
+		    self.new_bk_source[index] = self.new_bk_source[index].replace('+', '')
+		    self.new_bk_source[index] = self.new_bk_source[index].replace('-', '')
+
+		for index in range(0, len(self.new_bk_target)):
+		    self.new_bk_target[index] = self.new_bk_target[index].replace('+', '')
+		    self.new_bk_target[index] = self.new_bk_target[index].replace('-', '')
+
+	def get_branch(self, curr_value, next_value):
+		if curr_value == '': 
+			return next_value
+		return '{},{}'.format(curr_value, next_value)
+
+	def get_bk_pred(self, bk, tree):
+		pass
+
+	def generate_map(self, individual_tree):
+		tree_source = self.define_individuals(individual_tree)
+
+
+	def get_modes(self, structured_tree, pred_source):
+		#LEVA EM CONSIDERAÇAO QUE SÓ HÁ UM PREDICADO EM CADA NÓ
+		new_pred_source = '{}('.format(pred_source.split('(')[0])
+		if not self.variables:
+			self.variables.extend(("".join(re.findall('\((.*?)\)', structured_tree[0]))).split(', '))
+		tmp_var = ("".join(re.findall('\((.*?)\)',pred_source))).split(',')
+		for var in tmp_var:
+			if var in self.variables:
+				new_pred_source += '+,'
+			else: 
+				new_pred_source += '-,'
+				self.variables.append(var)
+		return '{})'.format(new_pred_source) 
+
+
+	def get_valid_predicates(self, pred_source):
+		#list_pred é : list_pred = [('movie', '+,-'), ('director', '+'),...]
+		#pred_source é: 'movie(+person,-person).' --> NAO VEM ASSIM: verificar quais variáveis já estão na árvore, se já tiver a variável, é +, cc -
+		#VERIFICAR CASO ONDE HÁ DOIS PREDICADOS NO PRED_SOURCE
+		occur_modes = ','.join([pred_source[occur.start()] 
+			               for occur in re.finditer('[+\-]', pred_source)])
+		valid_pred = []
+		for pred, mode in self.pred_target:
+			if mode == occur_modes: valid_pred.append(pred)
+		return valid_pred
+
+	def change_pred(self, source_pred, target_pred):
+		#pegar os parâmetros do source_pred e passar para o target_pred
+		return '{}({}'.format(target_pred.split('(')[0], source_pred.split('(')[1])
+
+	def define_individual(self, structured_tree, tree_number):
+		individual_tree = []
+		target = structured_tree[0]
+		nodes = structured_tree[1]
+		for values, node in nodes.items():
+			if values == '': 
+				branch = '{} :- {}.'.format(target, node)
+			else: branch = '{}.'.format(node)
+			left_branch = 'true' if self.get_branch(values, 'true') in nodes  else 'false'
+			right_branch = 'true' if self.get_branch(values, 'false') in nodes else 'false'
+			individual_tree.append('{};{};{};{};{}'.format(tree_number, values, 
+														branch, left_branch, right_branch))
+		return individual_tree
+
+	def define_individuals(self, structured_trees):
+		individual_trees = []
+		for index in range(0, len(structured_trees)):
+			individual_trees.extend(self.define_individual(structured_trees[index], index))
+		return individual_trees
+
+	def generate_random_individual(self, structured_tree):
+		new_tree = []
+		new_tree.append(structured_tree[0])
+		for values, node in structured_tree[1].items():
+			#mode: pegar do bk
+			#num_variaveis: pegar do bk
+			# guardar do anterior para pegar no proximo e nao jogar fora a list_pred
+			#list_pred = [movie(+movie, -person), director(+person)] e, ao pegar mode e num_variaveis
+			# pegar apenas os predicados que casam fazendo uma variavel allowed_pred
+			#choice = random em allowed_pred
+			#trocar allowed_pred[choice] e node
+			pred_source = self.get_modes(structured_tree, node)
+			valid_preds = self.get_valid_predicates(pred_source)
+			index_choice = randint(0, len(valid_preds)-1)
+			new_node = self.change_pred(node, valid_preds[index_choice])
+			self.mapping_transfer(node, valid_preds[index_choice])
+			structured_tree[1][values] = node.replace(node, new_node)
+		return structured_tree
+
+	def generate_random_individuals(self):
+		individual_structured_trees = []
+		for index in range(0, len(self.tree_source)):
+			structured_tree = copy.deepcopy(self.tree_source[index])
+			individual_structured_trees.append(self.generate_random_individual(structured_tree))
+		return individual_structured_trees
+
+	def evaluate(self, individual_tree, target):
+		pass
+		# mapping_preds = self.generate_map(individual_tree)
+		# background_tr = boostsrl.modes(self.bk_target, [target], useStdLogicVariables=False)
+		# model_tr = boostsrl.train(background_tr, train_pos_target, train_neg_target, train_facts_target, refine=individual_tree, transfer=mapping_preds, trees=trees_number)
+		#fazer teste e retornar
+
+	def construct_individual(self):
+		self.generate_new_preds()
+		structured_trees = self.generate_random_individuals()
+		individual_trees = self.define_individuals(structured_trees)
+		return individual_trees
+
+  	#gerar mapeamento entre predicados
+  	#passar source original e o structured_trees modificados. pegar onde foi modificado e 
+  	#fazer setMap.
+  	#se forem iguais, modificar no refine para pred'
+
+			
+
---
+++ src/population.py
@@ -0,0 +1,37 @@
+from src.individual import *
+from deap import tools
+from deap import base
+from deap import creator
+
+
+class Population:
+
+	def __init__(self, pop_size=5):
+		self.population = []
+		self.pop_size = pop_size
+		self.toolbox = base.Toolbox()
+		# self.toolbox.register("mate", tools.cxTwoPoint) 
+
+	def construct_population(self, tree_source, bk_source, bk_target, pred_target):
+		for index in range(self.pop_size):
+
+			creator.create("FitnessMax", base.Fitness, weights=(-1.0,))
+			creator.create("Individual", Individual, fitness=creator.FitnessMax)
+			tmp = creator.Individual(tree_source, bk_source, bk_target, pred_target)
+
+
+			# self.toolbox.register("function", tmp.constructIndividual, list_flags)
+			self.toolbox.register("function", tmp.construct_individual)
+			self.population.append(self.toolbox.function())
+
+	def print_pop(self):
+		print ('['),
+		for ind in self.population:
+			print (ind)
+		print (']')
+
+
+
+
+
+
