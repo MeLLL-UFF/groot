cb883ebe8dca3e8e002c706f702c7fa37bec24a2
lefreire <letfreirefigueiredo@gmail.com>
2021-01-27 17:34:25 -0300

    Making some modifications

--- src/experiments/aux_code.py
+++ src/experiments/aux_code.py
@@ -1,28 +1,65 @@
 ####
 # Auxiliary source
 ####
+from boostsrl import boostsrl
+import copy
+import json
 import re
-
+import os
+import random
+import numpy as np
+import sys
+from sklearn.model_selection import KFold, train_test_split
 
 def get_train_division(dataset):
     train_facts_source = []
     train_pos_source = []
     train_neg_source = []
-    for i in range(0, len(dataset)):
+    for i in range(0, len(dataset[0])):
         train_facts_source.extend(dataset[0][i])
         train_pos_source.extend(dataset[1][i])
         train_neg_source.extend(dataset[2][i])
     return train_facts_source, train_pos_source, train_neg_source
 
 
-def get_train_test(dataset):
-    train_facts = dataset[0][1:]
-    train_pos = dataset[1][1:]
-    train_neg = dataset[2][1:]
+def split_folds(dataset, n_folds):
+    random.shuffle(dataset)
+    res = []
+    kf = KFold(n_splits=n_folds)
+    for train_index, test_index in kf.split(dataset):
+        res.append(np.array(dataset)[test_index])
+    return res
 
-    test_facts = dataset[0][0]
-    test_pos = dataset[1][0]
-    test_neg = dataset[2][0]
+
+def split_train_test(dataset, test_size=0.3):
+    X_train, X_test = train_test_split(dataset, test_size=test_size, random_state=42)
+    return X_train, X_test
+
+def get_train_test(dataset):
+    if len(dataset[0]) >= 3:
+        train_facts = dataset[0][1:]
+        train_pos = dataset[1][1:]
+        train_neg = dataset[2][1:]
+
+        test_facts = dataset[0][0]
+        test_pos = dataset[1][0]
+        test_neg = dataset[2][0]
+    elif len(dataset[0]) == 2:
+        test_facts = dataset[0][0]
+        test_pos = dataset[1][0]
+        test_neg = dataset[2][0]
+
+        train_facts = split_folds(dataset[0][1], 2)
+        train_pos = split_folds(dataset[1][1], 2)
+        train_neg = split_folds(dataset[2][1], 2)
+    else:
+        train_facts, test_facts = split_train_test(dataset[0][0])
+        train_pos, test_pos = split_train_test(dataset[1][0])
+        train_neg, test_neg = split_train_test(dataset[2][0])
+
+        train_facts = split_folds(train_facts, 2)
+        train_pos = split_folds(train_pos, 2)
+        train_neg = split_folds(train_neg, 2)
     
     return train_facts, train_pos, train_neg, test_facts, test_pos, test_neg
 
@@ -64,10 +101,10 @@ def create_structured_trees(model):
     return structured_src, new_src_struct
 
 
-def create_pred_target(bk):
+def create_pred_target(kb):
     #pred_target é : pred_target = [('movie', '+,-'), ('director', '+'),...]
     pred_target = []
-    for pred in bk:
+    for pred in kb:
         modes = ','.join([pred[occur.start()] for occur in re.finditer('[+\-]', pred)])
         pred_target.append((pred.split('(')[0], modes))
     return pred_target
@@ -83,22 +120,26 @@ def get_best_individual(population):
     return ind
     
 
-def get_refine_transfer(ind, source, target):
+def get_refine_transfer(ind, source, target, kb_source, kb_target):
+
+    if not os.path.exists(f'src/experiments/{kb_source}_{kb_target}'):
+            os.makedirs(f'src/experiments/{kb_source}_{kb_target}')
+
     refine = []
     for tree in ind.modified_src_tree:
         refine.extend(tree)
     transfer = ind.transfer.transfer
-    with open(f'src/experiments/imdb_cora/refine_{source}_{target}.txt', 'w') as f:
+    with open(f'src/experiments/{kb_source}_{kb_target}/refine_{source}_{target}.txt', 'w') as f:
         f.write(json.dumps(refine))
         f.close()
-    with open(f'src/experiments/imdb_cora/transfer_{source}_{target}.txt', 'w') as f:
+    with open(f'src/experiments/{kb_source}_{kb_target}/transfer_{source}_{target}.txt', 'w') as f:
         f.write(json.dumps(transfer))
         f.close()
     return refine, transfer
 
 
-def test_refine_transfer(bk, target, refine, transfer, train_dataset, test_dataset):
-    background_train = boostsrl.modes(bk, [target], useStdLogicVariables=False, 
+def test_refine_transfer(kb, target, refine, transfer, train_dataset, test_dataset):
+    background_train = boostsrl.modes(kb, [target], useStdLogicVariables=False, 
                                           maxTreeDepth=3, nodeSize=2, numOfClauses=8)
     model_tr = boostsrl.train(background_train, train_dataset[0], train_dataset[1], 
                                       train_dataset[2], refine=refine, transfer=transfer,
@@ -108,7 +149,7 @@ def test_refine_transfer(bk, target, refine, transfer, train_dataset, test_datas
     return test_model.summarize_results(), test_model
 
 
-def test_tree_b(source, target, bk_source, bk_target, transferred_structured, train_dataset, test_dataset):
+def test_tree_b(source, target, kb_source, kb_target, transferred_structured, train_dataset, test_dataset):
     sys.path.insert(0, '../TreeBoostler')
 
     from revision import revision
@@ -123,7 +164,7 @@ def test_tree_b(source, target, bk_source, bk_target, transferred_structured, tr
     def print_function(message):
         global experiment_title
         global nbr
-        experiment_title = f'IMDB->cora: {source}->{target}'
+        experiment_title = f'Twitter->Yeast2: {source}->{target}'
         nbr = 1
         if not os.path.exists('experiments/' + experiment_title):
             os.makedirs('experiments/' + experiment_title)
@@ -131,26 +172,35 @@ def test_tree_b(source, target, bk_source, bk_target, transferred_structured, tr
             print(message, file=f)
             print(message)
     
-    tr_file = transfer.get_transfer_file(bk_source, bk_target, predicate, to_predicate, searchArgPermutation=True, allowSameTargetMap=False)
+    tr_file = transfer.get_transfer_file(kb_source, kb_target, predicate, to_predicate, searchArgPermutation=True, allowSameTargetMap=False)
     new_target = to_predicate
     
     # transfer and revision theory
-    experiment_title = f'IMDB->cora: {source}->{target}'
-    nbr = 1
-    background = boostsrl.modes(bk_target, [to_predicate], useStdLogicVariables=False, maxTreeDepth=3, nodeSize=2, numOfClauses=8)
+    # experiment_title = f'IMDB->cora: {source}->{target}'
+    # nbr = 1
+    background = boostsrl.modes(kb_target, [to_predicate], useStdLogicVariables=False, maxTreeDepth=3, nodeSize=2, numOfClauses=8)
     return revision.theory_revision(background, boostsrl, target, train_dataset[0], train_dataset[1], train_dataset[2], test_dataset[0], test_dataset[1], test_dataset[2], transferred_structured, transfer=tr_file, trees=10, max_revision_iterations=1, print_function=print_function)
 
 
-def save_results(final_results, source, target):
-    with open(f'src/experiments/imdb_cora/groot_train_{source}_{target}.txt', 'w') as f:
-        f.write(json.dumps(final_results[f'{source}->{target}']))
+def save_results(final_results, source, target, kb_source, kb_target):
+    if not os.path.exists(f'src/experiments/{kb_source}_{kb_target}'):
+            os.makedirs(f'src/experiments/{kb_source}_{kb_target}')
+
+    with open(f'src/experiments/{kb_source}_{kb_target}/groot_train_{source}_{target}.txt', 'w') as f:
+        f.write(json.dumps(final_results[f'{source}->{target}'][1]))
+        f.write('\n')
+        f.write(json.dumps(final_results[f'{source}->{target}'][2]))
         f.close()
-    with open(f'src/experiments/imdb_cora/groot_test_{source}_{target}.txt', 'w') as f:
+    with open(f'src/experiments/{kb_source}_{kb_target}/groot_test_{source}_{target}.txt', 'w') as f:
         f.write(json.dumps(final_results[f'groot_test:{source}->{target}']))
         f.close()
-    with open(f'src/experiments/imdb_cora/groot_inf_{source}_{target}.txt', 'w') as f:
+    with open(f'src/experiments/{kb_source}_{kb_target}/groot_inf_{source}_{target}.txt', 'w') as f:
         f.write(json.dumps(final_results[f'inf_res:{source}->{target}']))
         f.close()
-    with open(f'src/experiments/imdb_cora/treeb_test_{source}_{target}.txt', 'w') as f:
-        f.write(json.dumps(final_results[f'tree_test:{source}->{target}']))
+    with open(f'src/experiments/{kb_source}_{kb_target}/treeb_test_{source}_{target}.txt', 'w') as f:
+        f.write(json.dumps(final_results[f'tree_test:{source}->{target}'][1]))
+        f.write('\n')
+        f.write(json.dumps(final_results[f'tree_test:{source}->{target}'][2]))
+        f.write('\n')
+        f.write(json.dumps(final_results[f'tree_test:{source}->{target}'][2]))
         f.close()
